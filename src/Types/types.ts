234b328d9ca5594ac74c5b3815a70f17a79d5c08
Jared Truscott <jared@jaredmakes.com>
2024-09-29 10:46:30 -0400

    more tweaks

--- src/Types/types.ts
+++ src/Types/types.ts
@@ -72,4 +72,4 @@ export type Blog = {
 export type Docs = Blog;
 
 // This type is auto-generated. Do not edit manually.
-export type ThemeNames = 'theme-light' | 'theme-dark' | 'theme-Alkaline' | 'theme-1337' | 'theme-blue-steel' | 'theme-Cthulhu-fhtagn' | 'theme-haunted' | 'theme-neon-bliss' | 'theme-old-couch' | 'theme-peppermint' | 'theme-pop-punk' | 'theme-slime' | 'theme-spring' | 'theme-syntax';
+export type ThemeNames = 'theme-light' | 'theme-dark' | 'theme-deep-sea' | 'theme-generic' | 'theme-haunted' | 'theme-invisible' | 'theme-leet' | 'theme-neon-bliss' | 'theme-old-couch' | 'theme-peppermint' | 'theme-pop-punk' | 'theme-slime' | 'theme-spring' | 'theme-syntax';
